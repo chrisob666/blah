c32e47615c71f7eb8bbbbd11ff6966b5f2ac99ce
Christopher O'Brien <chrisob91@gmail.com>
2013-08-09 00:22:11 -0500

    log testing

--- applications/init/controllers/default.py
+++ applications/init/controllers/default.py
@@ -2,5 +2,6 @@ import logging
 logger = logging.getLogger('web2py.app.'+request.application)
 logger.setLevel(logging.DEBUG)
 def index(): 
+	logger.debug('yo')
 	return "isn't nothing but a gee thing."
 	return BEAUTIFY(request)
