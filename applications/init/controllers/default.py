8eb0edd2ee6a8612691bcb29cd7a26ef0c53d605
Christopher O'Brien <chrisob91@gmail.com>
2013-08-09 00:09:18 -0500

    logging now uses stdout and syslog, and routes error messages to simple pages

--- applications/init/controllers/default.py
+++ applications/init/controllers/default.py
@@ -2,4 +2,5 @@ import logging
 logger = logging.getLogger('web2py.app.'+request.application)
 logger.setLevel(logging.DEBUG)
 def index(): 
+	return "isn't nothing but a gee thing."
 	return BEAUTIFY(request)
--- routes.py
+++ routes.py
@@ -1,3 +1,4 @@
+logging='debug'
 routes_onerror = [
 	(r'*/404', r'/init/static/404.html'),
 	(r'*/500', r'/init/static/500.html'),
